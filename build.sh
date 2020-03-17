044c564a27fcc920d757e72d4d3b280b4e68cf8a
arttttt <artem-bambalov@yandex.ru>
2020-03-17 22:07:41 +0300

    adapt build script for P

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 isInitialized=false
 
 # Patches
-BIONIC_LIBM_PATCH=69b21e2c72e904deaf5aa3e90ba3a3f63577540a
+BIONIC_LIBM_PATCH=9c48399beef7f0b1a1cd14e0353b322027763484
+FENCE_LEAKING_PATCH=7a3b1bae646b580fb67afa590f37eb9c5ed12403
 
 function applyPatches() {
-	cd ./bionic
+	root=$PWD
+	cd $root/bionic
 	git cherry-pick $BIONIC_LIBM_PATCH
-	cd ..
+	cd $root/hardware/interfaces
+	git fetch "https://github.com/LineageOS/android_hardware_interfaces" refs/changes/97/265297/5 && git cherry-pick FETCH_HEAD
+	cd $root
 }
 
 function initialization() {
@@ -21,6 +25,7 @@ function syncSources() {
 	echo
 	echo "applying necessary patches"
 	applyPatches
+	main
 }
 
 function compilePackage() {
